1d62688be38c5273e87c9d772067327b46a0199d
salkyn <omursalkyn@gmail.com>
2023-03-30 19:55:10 +0600

    added components 03 04

---
+++ src/03-test/03.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const  User1 =()=> {
+    return (
+        <div>
+            <h2> User1</h2>
+        </div>
+    )
+
+}
---
+++ src/04-test/04.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const  User =()=> {
+    return (
+       <div>
+           <h2> User</h2>
+       </div>
+    )
+
+}
+
+
+
+
+
